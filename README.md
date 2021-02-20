21fcbff6ca4750db89e9cbb6e503ee9218bccb38
231 <snu231@github.com>
2021-02-21 01:54:53 +0900

    Initialize the chrome_app

---
+++ README.md
@@ -0,0 +1,1 @@
+If you check weather in your current position, you need to allow the site to read your position.
---
+++ todo.js
@@ -0,0 +1,77 @@
+const toDoForm = document.querySelector(".js-todoForm"),
+toDoInput = toDoForm.querySelector("input"),
+toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+let toDos = [];
+
+function deleteToDo(event){
+
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+
+    const cleanTodos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanTodos;
+    saveToDos();
+}
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length+1;
+    delBtn.innerText = "X";
+
+    delBtn.addEventListener("click", deleteToDo);
+    
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.id = newId;
+
+    toDoList.appendChild(li);
+
+    const toDoOjb = {
+        text: text,
+        id: newId,
+    };
+    toDos.push(toDoOjb);
+    saveToDos();
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value ="";
+}
+
+function loadToDos(){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        }
+        );
+    }
+    
+}
+
+
+function init(){
+
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+
+
+init();
